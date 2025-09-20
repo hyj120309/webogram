2e0282f341e4f99d61e5dde2e91e00e65bb6585c
hyj120309 <123804488+hyj120309@users.noreply.github.com>
2025-09-20 15:30:54 +0800

    Update config.js

--- app/js/lib/config.js
+++ app/js/lib/config.js
@@ -18,7 +18,7 @@ Config = window.Config || {}
 */
 
 Config.App = {
-  id: 29251291,
+  id: '29251291',
   hash: 'e7158e3fcffa32bca10c59b8a25c7323 ',
   version: '0.7.0',
   domains: ['tg.aliyun.com.hyj120309.top' ,'hyj120309.github.io']
